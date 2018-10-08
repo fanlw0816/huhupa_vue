389b48854fc4399bd243e6e682004fa13115872e
zhu yong jun <tmzyj2014@163.com>
2018-10-09 00:48:38 +0800

    调整样式

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App'
 import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
+import BootstrapVue from 'bootstrap-vue'
+import './styles/index.scss'
+// import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/styles/theme-chalk/index.css'
 
+Vue.use(BootstrapVue)
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
